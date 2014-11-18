0fc8e7a78cdd0416181ef4dfbc7055e14201e314
johngruen <john.gruen@gmail.com>
2014-11-18 12:49:51 -0500

    initial commit

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -2,9 +2,11 @@ module Authentication
   extend ActiveSupport::Concern
 
   def current_user=(user)
+    return User.first
     cookies.signed[:user_id] = { value: user.id, expires: 1.month.from_now }
 
     @current_user = user
+
   end
 
   def current_user
